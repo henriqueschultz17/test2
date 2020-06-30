79993ec89f2bbd9f4f2cc7b8f925b17b589bd786
henriqueschultz17 <67636158+henriqueschultz17@users.noreply.github.com>
2020-06-30 16:53:48 +0100

    boilerplate done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./styles.css";
+
 class Square extends React.Component {
   render() {
     return <button className="square">{/* TODO */}</button>;
